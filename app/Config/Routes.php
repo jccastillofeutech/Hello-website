26f298741e14e0945a27ce955f02970679c251b8
jccastillofeutech <ultraorenone@gmail.com>
2026-09-10 23:18:24 +0800

    Set custom homepage as root route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/', 'Pages::home');
 $routes->get('/home', 'Pages::home');
 $routes->get('/about', 'Pages::about');
 $routes->get('/customers', 'Customers::customers');
